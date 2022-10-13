51f245f90d3a4ea02e2d56593dc36172e8c00df7
Obd2004 <obidjonabd1097@gmail.com>
2022-10-13 10:01:48 +0500

    js home-third

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var hello = alert ("Xush kelibsiz!")
+var name1 = prompt ("Ismingizni kiriting:")
+var surName = prompt ("familyangizni kiriting:")
+var num = prompt ("Telefon raqamingizni kiriting:")
+
+console.log(hello);
+console.log(`Sizning ismingiz ${name1}`);
+console.log(`Sizning familyangiz ${surName}`);
+console.log(`Sizning raqamingiz ${num}`);
